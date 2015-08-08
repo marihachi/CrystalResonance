af8832c5ef421754695e202c0299023f41fcec0b
syuilo <syuilotan@yahoo.co.jp>
2015-08-09 04:43:43 +0900

    Improve semanticability

--- src/application/views/top.php
+++ src/application/views/top.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
-<html lang=jp>
+<html lang=ja>
 	<head>
 		<meta charset=UTF-8 />
 		<meta http-equiv=X-UA-Compatible content="IE=edge" />
